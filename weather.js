560e6780eda585b3695ef1db66564dbe0c2b40d1
krelith <krelithsg@gmail.com>
2018-02-11 00:42:24 +0000

    weather api improvements

--- weather.js
+++ weather.js
@@ -1,40 +1,30 @@
-let clear = false;
-let cloudy = false;
-let storm = false;
+let weather_id = 9999;
+let weather_id_check = true;
+let weather_category = "null";
+let weather_description = "null";
+let wind = 9999;
+let temp = 9999;
+let temp_check = true;
 
 function setup(){
     createCanvas(600, 600);
-}
-
-function preload(){
     let url = "https://api.openweathermap.org/data/2.5/weather?q=plymouth,uk&units=metric&APPID=842fbb38545fe9578a86fd7ce9dfb7f1";
     httpGet(url, 'jsonp', false, function(response){
-        let result = response.weather[0].id;
-        console.log(result);
-        if (result > 800){
-            cloudy = true;
-        } else
-        if (result == 800){
-            clear = true;
-        } else
-        if (result >= 200 && result < 300){
-            storm = true;
-        }
+        temp = response.main.temp;
+        weather_id = response.weather[0].id;
+        weather_category = response.weather[0].main;
+        weather_description = response.weather[0].description;
+        wind = response.wind.speed;
+        console.log("Weather category: " + weather_category);
+        console.log("Type of " + weather_category + ": " + weather_description);
+        console.log("Wind speed: " + wind + " metres/sec");
     });
 }
 
 function draw(){
     background(255);
-    if (cloudy == true){
-        console.log("The weather is cloudy");   
-        cloudy = false; // Prevents the call from occurring more than once due to draw loop.
-    } else 
-    if (clear == true){
-        console.log("The weather is clear");
-        clear = false;
-    }
-    if (storm == true){
-        console.log("The weather is stormy");
-        stormy = false;
+    if (temp_check == true && temp != 9999){
+        console.log("Fo real the temp is " + temp);
+        temp_check = false;
     }
 }
